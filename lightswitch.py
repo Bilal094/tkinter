dbc423995d501cb6a5f8251ce18e0a2cbe56360b
Bilal094 <99066318@mydavinci.nl>
2022-03-06 17:22:04 +0100

    afgemaakt

--- lightswitch.py
+++ lightswitch.py
@@ -5,9 +5,18 @@ button = tk.Button(text='...', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
-#test
+def switch():
+    def switch2():
+        window.config(bg='black')
+        button.config(text= 'Switch light on', command=switch)
+        print('light is off')
+    window.config(bg='yellow')
+    button.config(text= 'Switch light off', command=switch2)
+    print('light is on')
 
-
+print('light is off')
+window.config(bg='black')
+button.config(text= 'Switch light on', command=switch)
 # schijf hier tussen je code
 
 window.mainloop()
